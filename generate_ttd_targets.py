755c5f15c1a35d7295f0dd42d68a7c30f96ee397
Ralph Kube <ralph_kube@gmx.net>
2023-03-07 15:25:24 -0500

    Fixing dtype of target_ttd to np.float32

--- generate_ttd_targets.py
+++ generate_ttd_targets.py
@@ -29,10 +29,23 @@ with open(args.dataset_def, 'r') as fp:
 
 
 for shotnr in dataset_def["shots"].keys():
-    print(shotnr)
     # Iterate over the target variables and find the longest time base
     # of the signals. Use this timebase to generate a ttd target
     with h5py.File(join(args.destination, f"{shotnr}.h5"), "a") as df:
+
+        if dataset_def["shots"][shotnr]["ttd"] > 0.0:
+            # Shot is disruptive, target is now a count-down
+            tb = np.arange(0.0, dataset_def["shots"][shotnr]["ttd"], 1.0, dtype=np.float32)
+            target = tb.max() - tb
+            target = np.clip(target, 0.0, dataset_def["ttd_max"])
+        else:
+            # Shot is not disruptive. TTD is ttd_max (defined in yaml file)
+            tb = np.arange(0.0, df.attrs["tmax"], 1.0)
+            target = dataset_def["ttd_max"] * np.ones_like(tb)        
+
+        target = np.log10(target + 0.1)
+
+        logging.info(f"target_ttd = {target.shape}")
 
         try:
             if df["target_ttd"]["xdata"].size > 0:
@@ -41,23 +54,11 @@ for shotnr in dataset_def["shots"].keys():
         except KeyError:
             pass
 
-        # Generate a time-to-disruption target, based on 1ms samples
-        tb = np.arange(0.0, df.attrs["tmin"], 1.0)
-
-        if dataset_def["shots"][shotnr]["ttd"] < 0.0:
-            # Shot is disruptive, target is now a count-down
-            target = tb.max() - tb
-            target = np.clip(target, 0.0, dataset_def["ttd_max"])
-        else:
-            # Shot is not disruptive. TTD is ttd_max (defined in yaml file)
-            target = dataset_def["ttd_max"] * np.ones_like(tb)        
-        target = np.log10(target + 0.1)
-
-        logging.info(f"target_ttd = {target}")
-
+        # This is only executed if the try/except block abvove passes
+        # Write new ttd into hdf5 file
         grp_t = df.create_group("target_ttd")
-        grp_t.create_dataset("xdata", data=tb)
-        grp_t.create_dataset("zdata", data=target)
+        grp_t.create_dataset("xdata", data=tb.astype(np.float32))
+        grp_t.create_dataset("zdata", data=target.astype(np.float32))
 
 
 
