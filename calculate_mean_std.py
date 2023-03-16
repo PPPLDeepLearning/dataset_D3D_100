b781b65138b3f4a657cf276184fce4460d5cbfeb
Ralph Kube <ralph_kube@gmx.net>
2023-03-16 09:11:17 -0400

    Adding normalization of profiles, and min/max values

--- calculate_mean_std.py
+++ calculate_mean_std.py
@@ -11,42 +11,56 @@ import h5py
 
 
 
+"""
+This script calculates max, min, mean, and std individually for all signals in a dataset definition.
+Max, min, mean, and std are calculated over all shots, and for each signal individually.
+For 1d signals, all channels in the signal are used. That is, the calculation is over all shots
+and all channels.
+
+The results are stored in the file `normalization.yaml` as follows
+
+predictor_name:
+    max:
+    mean:
+    min:
+    std:
+
+"""
 
 
-# Each task fetches a single variable from all shots and calculates the channel-wise
+# Each task fetches a single variable from all shots and calculates the 
 # mean and standard deviation.
 
 
 def process_variable(args):
-    """Fetch a single variable from all shots and calculate the channel-wise mean and std.
+    """Fetch a single variable from all shots and calculate the channel-wise min, max, mean and std.
 
     args:
         datadir (string): Directory to HDF5 files
-        group_name (string): HDF5 group name for the variable to calculate mean,std for
+        group_name (string, string) : Tuple (predictor_name, map_to). This is used to find the HDF5
+                                      group name for the predictor. 
         shotlist (list[Int]): List of shots to process
 
     """
-    datadir, group_name, shotlist = args
+    datadir, group_name, shotlist = args    # Unpack the arguments
+    pred_name, map_to = group_name          # Unpack predictor name and map_to
     # 1. Cache the relevant data from list of hdf5 files
     cache_data = []
     for shotnr in shotlist:
         with h5py.File(join(datadir, f"{shotnr}.h5"), "r") as df:
-            cache_data.append(df[group_name]["zdata"][:])
+            cache_data.append(df[map_to]["zdata"][:])
 
-    #for signal in cache_data:
-    #    print(signal.shape)
     all_signals = np.hstack(cache_data)
 
 
-    # 2. Calculate mean and std. Convert numpy datatypes to float. Otherwise yaml.safe_dump
+    # 2. Calculate min, max, mean and std. Convert numpy datatypes to float. Otherwise yaml.safe_dump
     #    throws an error
-    return {group_name: {"mean": float(all_signals.mean()), "std": float(all_signals.std())}}
+    return {pred_name: {"min": float(all_signals.min()),
+                        "max": float(all_signals.max()),
+                        "mean": float(all_signals.mean()), 
+                        "std": float(all_signals.std())}}
 
     
-def hello(args):
-    ix, pred, shot_list = args
-    print(f"Hello, ix={ix}, pred={pred}, shot_list={shot_list}")
-
 if __name__ == "__main__":
     print("main")
 
@@ -71,32 +85,41 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-
+    # Load dataset definition
     with open(args.dataset_def, "r") as stream:
         dataset_def = yaml.safe_load(stream)
 
+    # The MDS/PTdata signals are stored under the field `map_to` in signals?.yaml
+    # Load the yaml files and find the map_to names that correspond to the field 'predictors' in the
+    # dataset definition
+
+    # Load mappings for 0d signals
     with open(args.signal_defs_0d, "r") as stream:
         signals_0d = yaml.safe_load(stream)
+    # Load mappings for 1d (profile) signals
+    with open(args.signal_defs_1d, "r") as stream:
+        signals_1d = yaml.safe_load(stream)
+
+    # Gather the name of the predictor and the corresponding HDF5 group names 
+    # of the predictors listed in the definition of the dataset.
+    # The result is a list of tuples (predictor, map_to)
+    group_list_0d = [(k, signals_0d[k]["map_to"]) for k in dataset_def["predictors"] if k in signals_0d.keys()]
+    group_list_1d = [(k, signals_1d[k]["map_to"]) for k in dataset_def["predictors"] if k in signals_1d.keys()]
+    group_list = group_list_0d + group_list_1d
+
 
     # Generate list from shots in the dataset
     shot_list = list(dataset_def["shots"].keys())
 
-    # The data is not stored by the alias of the predictor, but under the field `map_to` in signals?.yaml
-    # Load the yaml files and find the map_to names that correspond to the field 'predictors' in the 
-    # dataset definition yaml file
-    group_list = [signals_0d[k]["map_to"] for k in dataset_def["predictors"]]
-
-
     pool = mp.Pool(12)
     res_mean_std = pool.map(process_variable,  [("D3D_100", grp, shot_list) for grp in group_list])
 
-    # all_mean_std is a list of dicts: [{"var1": (mean, std)}, {"var2": (mean, std)}]
+    # all_mean_std is a list of dicts: [{"var1": (min, max, mean, std)}, {"var2": (min, max, mean, std)}, ...]
     # combine them into a single dict
     dict_mean_std = {}
     for i in res_mean_std:
         dict_mean_std.update(i)
-
-    
+    # Serialize the dictionary to file
     with open("normalization.yaml", "w") as fp:
         fp.write(yaml.safe_dump(dict_mean_std))
     
